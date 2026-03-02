22ab14a25be64c3720c994cfbc05cbd57fdf228a
prudhvinathreddy1904 <mprudhvinathreddy7777@gmail.com>
2026-03-02 16:21:16 +0530

    Added level3.1.java

---
+++ level3.1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheitResult = (celsius * 9/5) + 32;
+
+        System.out.println("The " + celsius + " celsius is " 
+                + fahrenheitResult + " fahrenheit");
+
+        sc.close();
+    }
+}
